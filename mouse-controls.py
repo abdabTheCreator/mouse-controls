deb1bbbe28c31e6ccbff3f9052a2d33ff31a6d34
Abby <59022024+abdabTheCreator@users.noreply.github.com>
2023-06-29 18:47:48 +1200

    Update mouse-controls.py

--- mouse-controls.py
+++ mouse-controls.py
@@ -25,7 +25,7 @@ def on_key_press(key):
         up_pressed = True
     elif key == keyboard.Key.down:
         down_pressed = True
-    elif hasattr(key, 'char') and key.char == '.':
+    elif key == keyboard.Key.enter:
         pyautogui.rightClick()
 
 # handle arrow key releases
