9a67ca4bfa35d7076f1e968ad54cf4cc0b165a03
Abby <59022024+abdabTheCreator@users.noreply.github.com>
2023-07-01 15:20:48 +1200

    Update mouse-controls.py

--- mouse-controls.py
+++ mouse-controls.py
@@ -27,7 +27,7 @@ def on_key_press(key):
         down_pressed = True
     elif key == keyboard.Key.enter:
         pyautogui.mouseDown()
-    elif key == keyboard.Key.char == 'l':
+    elif key == key.char == 'l':
         pyautogui.leftClick()
        
     #elif key == keyboard.Key.enter:
