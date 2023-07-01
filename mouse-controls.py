b9e6c539f3fedd0e804520271de5222269ca82f7
Abby <59022024+abdabTheCreator@users.noreply.github.com>
2023-07-01 15:34:37 +1200

    Update mouse-controls.py

--- mouse-controls.py
+++ mouse-controls.py
@@ -27,7 +27,7 @@ def on_key_press(key):
         down_pressed = True
     elif key == keyboard.Key.enter:
         pyautogui.mouseDown()
-    elif key == keyboard.KeyCode.from_char('k'):
+    elif key == keyboard.KeyCode.from_char('/'):
         pyautogui.rightClick()
        
     #elif key == keyboard.Key.enter:
@@ -48,7 +48,7 @@ def on_key_release(key):
         down_pressed = False
     elif key == keyboard.Key.enter:
         pyautogui.mouseUp()
-    elif key == keyboard.KeyCode.from_char('k'):
+    elif key == keyboard.KeyCode.from_char('/'):
         pyautogui.mouseUp(button='right')
 
 # start key listener
